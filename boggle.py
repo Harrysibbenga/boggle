b9b8ecbd7c8d47c0d94bcaccb3b4955a874cf248
Harry Sibbenga <sibbengaharry@gmail.com>
2018-10-11 17:18:30 +0000

    test for letters in the grid

--- boggle.py
+++ boggle.py
@@ -1,8 +1,11 @@
+from string import ascii_uppercase
+from random import choice
+
 def make_grid(width, height):
     '''
     creates a grid that will hold all the tiles for boggle game
     
     '''
-    return {(row, col): ' ' for row in range(height)
-        for col in range(width)
-    } 
+    return { (row, col): choice(ascii_uppercase) 
+        for row in range(height)
+        for col in range(width) } 
